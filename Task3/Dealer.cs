a4ef0649de656f250c533b88f069556165d110d5
muhammadahmadcheema <ahmedcheema08@gmail.com>
2025-04-14 01:09:38 +0500

    PD_08

---
+++ Task3/Dealer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Dealer: Player
+    {
+        public override void AddCard(Card card)
+        {
+            base.AddCard(card);
+        }
+
+        public bool ShouldDraw()
+        {
+            return CalculateScore() < 17;
+        }
+    }
+}
